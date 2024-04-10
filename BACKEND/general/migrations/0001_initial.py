b08c2fa02cbcce844eb0b210ce57973bc11e9b79
Landmark-GitHub <l.landmark.mail@gmail.com>
2024-04-10 13:09:21 +0700

    Edit database name Product

--- BACKEND/general/migrations/0001_initial.py
+++ BACKEND/general/migrations/0001_initial.py
@@ -27,7 +27,6 @@ class Migration(migrations.Migration):
             fields=[
                 ('sku', models.AutoField(primary_key=True, serialize=False)),
                 ('name', models.CharField(max_length=100)),
-                # ('type', models.CharField(max_length=50)),
                 ('description', models.TextField()),
                 ('price', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('quantity', models.IntegerField()),
